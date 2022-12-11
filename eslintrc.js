490858e99f0909d50fe2dbe0d27a4e092d9dc91c
YeongMin Ahn <ahn0min95@gmail.com>
2022-12-11 18:11:59 +0900

    Chore: lint 관련 파일 추가

---
+++ eslintrc.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//   //주어진 구성을 확장, :recommended 내장된 "권장" 구성,
+//   // extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+//   extends: ["eslint:recommended", "plugin:react/recommended"],
+//   // eslint에게 타입스크립트인 것을 알려주고 린드구문해석을 하게 시키는 부분
+//   // parser: "@typescript-eslint/parser",
+//   // typescript 관련 플러그인을 정해주는 부분
+//   // plugins: ["@typescript-eslint", "react"],
+//   plugins: ["react"],
+//   root: true,
+//   env: {
+//     borwser: true,
+//   },
+// };
